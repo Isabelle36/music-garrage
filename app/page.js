fcf83ed0dfa9d21300cc2074e60bc1ba62351cba
Isabelle <155624058+Isabelle36@users.noreply.github.com>
2025-04-05 14:27:35 +0530

    pp

--- app/page.js
+++ app/page.js
@@ -1,15 +1,10 @@
 import React from 'react'
-import Piano from '@/components/ui/piano'
-import Metronome from '@/components/ui/metronome'
 
 const page = () => {
   return (
     <div className="container mx-auto p-4">
       <h1 className='text-3xl text-amber-300 mb-8'>Welcome to MusicBass!</h1>
-      <div className="mb-8">
-        <Metronome />
-      </div>
-      <Piano />
+      
     </div>
   )
 }
